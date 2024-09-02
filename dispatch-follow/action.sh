a05a7ce197349fcdd550e77cc2dccf393134d22a
Mateusz Budzisz <electroluxv2@gmail.com>
2024-09-02 19:25:33 +0200

    action test 9

--- dispatch-follow/action.sh
+++ dispatch-follow/action.sh
@@ -4,6 +4,7 @@ RUN_NAME="$INPUT_RUN_NAME - $(date)"
 echo -e "\x1b[35;49mDispatching \x1b[36;49m$INPUT_WORKFLOW_LOCATOR\x1b[35;49m with run name \x1b[36;49m$RUN_NAME"
 
 curl "https://api.github.com/repos/$INPUT_WORKFLOW_LOCATOR/dispatches" \
+  --no-progress-meter \
   -X POST \
   -H "Authorization: token $GITHUB_TOKEN" \
   -d '{
@@ -13,15 +14,16 @@ curl "https://api.github.com/repos/$INPUT_WORKFLOW_LOCATOR/dispatches" \
       "run-name": "'"$RUN_NAME"'"
     }
   }' \
-|& while read -r line; do echo -e "\x1b[90;49m$line"; done # Change curl output color to gray
+|& sed "s/^/\x1b[91;49m/" # Change curl output color to bright red
 
 for ((i = 1; i <=50; i++)); do        
   echo -e "\x1b[35;49mWaiting for dispatched run, attempt \x1b[36;49m$i\x1b[35;49m/\x1b[36;49m50"
 
   curl "https://api.github.com/repos/$INPUT_WORKFLOW_LOCATOR/runs" \
+    --no-progress-meter \
     -H "Authorization: token $GITHUB_TOKEN" \
     -o "$RUNS_TMP_FILE" \
-  |& while read -r line; do echo -e "\x1b[90;49m$line"; done # Change curl output color to gray
+  |& sed "s/^/\x1b[91;49m/" # Change curl output color to bright red
 
   # JQ will save empty file if run was not found
   jq '.workflow_runs[] | select(.name == "'"$RUN_NAME"'")' "$RUNS_TMP_FILE" > "$DISPATCHED_RUN_TMP_FILE"
